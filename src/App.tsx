698ac8fb50b69f91c11cbdb25b2d11a70665f32a
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-08-13 21:21:44 -0300

    :sparkles: | feat: adiciona configuracao inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <main className="bg-indigo-700 h-screen">
+      <h1 className="text-lg text-red-600">Hello world</h1>
+    </main>
+  )
+}
+
+export default App
