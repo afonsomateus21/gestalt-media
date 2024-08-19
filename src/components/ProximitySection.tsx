0c46b9c9e1810fdfecc27726be0961462f9a6305
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-08-19 14:48:01 -0300

    :sparkles: | feat: cria componente para representar a sequencia de circulos relacionados a lei da proximidade

---
+++ src/components/ProximitySection.tsx
@@ -0,0 +1,12 @@
+export function ProximitySection() {
+  return (
+    <div className="h-[150px] w-[200px] bg-blue-800 grid grid-rows-2 grid-flow-col gap-4 p-8">
+      <div className="size-9 rounded-full bg-white" />
+      <div className="size-9 rounded-full bg-white" />
+      <div className="size-9 rounded-full bg-white" />
+      <div className="size-9 rounded-full bg-white" />
+      <div className="size-9 rounded-full bg-violet-900" />
+      <div className="size-9 rounded-full bg-violet-900" />
+    </div>
+  );
+}
