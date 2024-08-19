63e7c2f18f200af6d9a082bd0a9952b59deaca76
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-08-19 14:53:06 -0300

    :recycle: | refac: refatora componente para utilizar o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <main className="bg-indigo-700 h-screen">
-      <h1 className="text-lg text-red-600">Hello world</h1>
+    <main className="bg-blue-800 h-screen flex items-center">
+      <Outlet />
     </main>
   )
 }
