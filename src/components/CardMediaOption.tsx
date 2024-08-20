06fb9e96333588c4153c1bf341b22c0aaabaea5c
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-08-19 23:43:21 -0300

    :sparkles: | feat: cria componente com card de escolha da midia

---
+++ src/components/CardMediaOption.tsx
@@ -0,0 +1,20 @@
+import { CirclePlay } from "lucide-react";
+import { CardMediaOptionProps } from "../interfaces/CardMediaOptionProps";
+
+export function CardMediaOption({ title, isBlue, image, ...props } : CardMediaOptionProps) {
+  return (
+    <button 
+      className={`${isBlue ? 'bg-blue-800' : 'bg-neutral-300'} relative w-64 h-80 rounded-3xl border-4 opacity-40 hover:opacity-100 hover:brightness-125 flex flex-col items-center justify-center p-2`}
+      {...props}
+    >
+      <strong className={`${isBlue ? 'text-neutral-300' : 'text-blue-800'} text-3xl mb-5`}>{ title }</strong>
+      <img src={ image } alt="" className="size-40" />
+      <div className={`absolute right-3 bottom-3 h-20 w-20 ${isBlue ? 'bg-neutral-300' : 'bg-blue-800'} rounded-3xl flex items-center justify-center`}>
+        <CirclePlay 
+          color={`${isBlue ? "#1e40af" : "#fff"}`}
+          size={60}
+        />
+      </div>
+    </button>
+  );
+}
