b0075c15d1925c0af0a568ef632fb37b51a7c6ae
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-08-19 23:41:31 -0300

    :lipstick: | style: adiciona ao tailwind.config o estilo de stroke no texto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,40 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        textShadow: {
+          'strong-stroke': `
+            4px 4px 2px #4C1D95,
+            -4px 4px 2px #4C1D95,
+            -4px -4px 0 #4C1D95,
+            4px -4px 0 #4C1D95,
+            0px 4px 2px #4C1D95,
+            0px -4px 2px #4C1D95,
+            4px 0px 2px #4C1D95,
+            -4px 0px 2px #4C1D95
+          `,
+        },
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.text-shadow-strong-stroke': {
+          textShadow: `
+            4px 4px 2px #4C1D95,
+            -4px 4px 2px #4C1D95,
+            -4px -4px 0 #4C1D95,
+            4px -4px 0 #4C1D95,
+            0px 4px 2px #4C1D95,
+            0px -4px 2px #4C1D95,
+            4px 0px 2px #4C1D95,
+            -4px 0px 2px #4C1D95
+          `,
+        },
+      });
+    },
+  ],
 }
 
