d40c60cd48fc9592abb67e697c5cfe7fb21bbd43
claralmora <clara.moura@cotabox.com.br>
2024-08-30 15:42:09 -0300

    ✨ | feat: adiciona como referência o site da disciplina sobre gestalt

--- src/constants/references.tsx
+++ src/constants/references.tsx
@@ -2,4 +2,5 @@ export const references = [
   "https://www.toptal.com/designers/ui/gestalt-principles-of-design#:~:text=There%20are%20six%20individual%20principles,order%20(also%20called%20pr%C3%A4gnanz).",
   "https://medium.com/@kinga.olszewska/how-game-interfaces-use-gestalt-psychology-fb77313a85fc",
   "https://uxcam.com/blog/netflix-appstudy/",
+  "https://ctd-smd.wixsite.com/cognosis2024-1/pragnanz",
 ]
