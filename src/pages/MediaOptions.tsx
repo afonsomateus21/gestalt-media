127132bfd5f5082c9ad264274d38c0f570c690a4
claralmora <clara.moura@cotabox.com.br>
2024-08-31 10:04:39 -0300

    🚧 | wip: chama componente 'CardGestaltConcepts'

--- src/pages/MediaOptions.tsx
+++ src/pages/MediaOptions.tsx
@@ -1,4 +1,5 @@
 import { CardMediaOption } from "../components/CardMediaOption";
+import { CardGestaltConcepts } from "../components/CardGestaltConcepts";
 import GameImage from "../assets/game-image.png";
 import CoverImage from "../assets/cover-image.png";
 import MovieImage from "../assets/movie-image.png";
@@ -6,9 +7,9 @@ import MovieImage from "../assets/movie-image.png";
 export function MediaOptions() {
   return (
     <div 
-      className="w-full h-screen flex items-center"
+      className="w-full h-full flex flex-col items-center"
     >
-      <div className="w-[1200px] h-[800px] mx-auto my-0 flex flex-col justify-evenly">
+      <div className="w-[1200px] h-screen mx-auto my-0 flex flex-col justify-evenly">
         <div className="w-full">
           <h1 className="font-extrabold text-[5rem] text-white text-shadow-extrastrong-violet-stroke animate-blink">Explorar</h1>
         </div>
@@ -44,6 +45,32 @@ export function MediaOptions() {
 
         <div className="ml-auto">
         <h1 className="font-extrabold text-[5rem] text-white text-shadow-extrastrong-blue-stroke animate-blink">Mídias</h1>
+        </div>
+      </div>
+
+      <div className="w-[1200px] h-full h-[1000px] mx-auto my-20 flex flex-col justify-evenly">
+        <div className="w-full">
+          <h1 className="font-extrabold text-[5rem] text-white text-shadow-extrastrong-violet-stroke  animate-blink">Explorar</h1>
+        </div>
+        
+        <div className="w-full flex justify-center gap-10 overflow-hidden">
+          <CardGestaltConcepts 
+            link="https://www.youtube.com/embed/8kQfT9k1Fuw"
+            title="Vídeo 01"
+          />
+
+          <CardGestaltConcepts 
+            link="https://www.youtube.com/embed/KSMvh0eiQtg"
+            title="Vídeo 02"
+          />
+
+          <CardGestaltConcepts 
+            link="https://www.youtube.com/embed/KSMvh0eiQtg"
+            title="Vídeo 03"
+          />
+        </div>
+        <div className="ml-auto">
+          <h1 className="font-extrabold text-[5rem] text-white text-shadow-extrastrong-blue-stroke animate-blink">Conceitos</h1>
         </div>
       </div>
     </div>
