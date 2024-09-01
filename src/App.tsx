39080ce2e4b92033fb6e192995bf7554b2d6a6ec
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-09-01 20:52:37 -0300

    :bug | fix: utiliza o ScrollRestoration para que a tela fique no topo sempre que houver uma mudanca de pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 function App() {
   return (
     <main className="bg-gradient-to-b from-blue-800 to-violet-900 h-auto">
       <Outlet />
+      <ScrollRestoration />
     </main>
   )
 }
